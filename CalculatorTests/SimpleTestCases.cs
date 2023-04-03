5da585195a93131f90f6c4c8fa0f9a57570b5a92
Segimara <76587473+Segimara@users.noreply.github.com>
2023-04-03 11:45:12 +0300

    Tasks 1- 3

---
+++ CalculatorTests/SimpleTestCases.cs
@@ -0,0 +1,64 @@
+using test_first_manner;
+using Xunit;
+
+namespace CalculatorTests
+{
+    public class SimpleTestCases
+    {
+        [Fact]
+        public void ZeroNums_0()
+        {
+            //Arrenge
+            StringCalculator calculator = new StringCalculator();
+            var testExprestion = "";
+            //Act 
+            var result = calculator.Add(testExprestion);
+            //Assert
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void OneNum_1()
+        {
+            //Arrenge
+            StringCalculator calculator = new StringCalculator();
+            var testExprestion = "1";
+            //Act 
+            var result = calculator.Add(testExprestion);
+            //Assert
+            Assert.Equal(1, result);
+        }
+        [Fact]
+        public void TwoNums_3()
+        {
+            //Arrenge
+            StringCalculator calculator = new StringCalculator();
+            var testExprestion = "1,2";
+            //Act 
+            var result = calculator.Add(testExprestion);
+            //Assert
+            Assert.Equal(3, result);
+        }
+        [Fact]
+        public void SevenNumbers_7()
+        {
+            //Arrenge
+            StringCalculator calculator = new StringCalculator();
+            var testExprestion = "1,1,1,1,1,1,1";
+            //Act 
+            var result = calculator.Add(testExprestion);
+            //Assert
+            Assert.Equal(7, result);
+        }
+        [Fact]
+        public void SevenNumbersWithNewLineDelimeter_6()
+        {
+            //Arrenge
+            StringCalculator calculator = new StringCalculator();
+            var testExprestion = "1,1\n1\n1,1\n1,1";
+            //Act 
+            var result = calculator.Add(testExprestion);
+            //Assert
+            Assert.Equal(7, result);
+        }
+    }
+}
---
+++ test-first-manner/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_first_manner
+{
+    public class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            return numbers.Split(',', '\n').Select(x =>
+            {
+                if (!string.IsNullOrEmpty(x))
+                {
+                    return int.Parse(x);
+                }
+                return 0;
+            })
+            .DefaultIfEmpty(0)
+            .Sum();
+        }
+    }
+}
